2e028a2a82908195a1302e853602c41d80942999
Mathieu <rivest.mathieu@gmail.com>
2013-02-07 17:02:41 -0500

    added rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wandrr::Application.config.secret_token = '1f911a8fb8856d7b2cdbf35fb58cd49ae2b04ee680c69401a8856a71fdbc4e10d3a8b076839c2869fc561125cae021a3fc205b85f435f51cc2bfb5fc5b50fe5e'
